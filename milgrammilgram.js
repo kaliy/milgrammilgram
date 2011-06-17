0bc1771e4e1e04aa60e57453f52ee506008d3907
Vitalii Samolovskikh <kefirfromperm@gmail.com>
2011-06-17 16:12:02 +0600

    q

--- milgrammilgram.js
+++ milgrammilgram.js
@@ -1,1 +1,14 @@
- 
+$(document).ready(
+    function(){
+        $('#texttext').keyup(
+            function(){
+                $('#result').text(milgram($('#texttext').val()));
+            }
+        );     
+    }
+);
+
+function milgram(source){
+    return source;
+}
+
